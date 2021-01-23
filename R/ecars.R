19004d03ff804d6ba9c549cae9b49aad4f942c8b
Yuan Zhou <zhouyuan7125@gmail.com>
2021-01-23 18:00:21 -0500

    create function and help

---
+++ R/ecars.R
@@ -0,0 +1,15 @@
+#' Calculate importance
+#'
+#' Assign importance value to each variable in the
+#' regression model.
+#'
+#' @param X A nxp data matrix
+#'
+#' @return A vector of each variable's importance value
+#' @export
+#'
+#' @examples
+#' ecars(X)
+ecars <- function(X){
+  return(1)
+}
