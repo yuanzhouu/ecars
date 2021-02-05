397e3e0b4d9e6ba5921fdbb6e413a07261554abc
Yuan Zhou <zhouyuan7125@gmail.com>
2021-02-05 00:02:36 -0500

    function working normally

--- R/alpha_est.R
+++ R/alpha_est.R
@@ -12,24 +12,41 @@
 #' @export
 #'
 #' @examples
-alpha_estimate <- function(X, Y, XXX_list,rsquare,samp_time,n_var=0){
+alpha_estimate <- function(X, Y, alpha=NULL, XXX_list=NULL,sigma2=NULL,samp_time=10,n_var=0){
+  if(is.null(alpha))
+  {
+    alpha <- -seq(0,1,length.out =21)
+  }
+
+  if(is.null(XXX_list))
+  {
+    XXX_list <- get_XXX_list(X, alpha)
+  }
+
+  if(is.null(sigma2))
+  {
+    sigma2 <- varest_gcv(X, Y)$sigma2
+    print(paste("sigma2 estimate:", sigma2))
+  }
   if(n_var==0)
   {
     X1 <- scale(X)
     lambda_min <- gcv_lambda_min(X,Y)
-    n_var <- sum(abs(glmnet(X, Y, lambda=lambda_min)$beta)>0)
-  }
+    n_var <- sum(abs(glmnet::glmnet(X, Y, lambda=lambda_min)$beta)>0)
+    print(paste("n_var estimate:", n_var))
+     }
+  #if n_var=0, stop
   # Estimate
   alpha_est <- 0
   for (i in 1:samp_time)
   {
     print(i)
     inf_index <- sample(1:ncol(X), n_var)
-    Y <- Y_sim(X, inf_index, rsquare,mode="uniform")
+    Y <- Y_sim(X, inf_index, sigma2,mode="uniform")
     pr_auc <- 0
     for(j in 1:length(XXX_list))
     {
-      Ecars_score <- abs(XXX_list[[j]] %*% scale(Y))/n
+      Ecars_score <- abs(XXX_list[[j]] %*% scale(Y))/length(Y)
       pr_auc[j] <- comp_auc(as.vector(Ecars_score),inf_index,p=1)
     }
     alpha_est[i] <- alpha[which.max(pr_auc)]
